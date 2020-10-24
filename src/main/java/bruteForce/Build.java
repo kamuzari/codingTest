45b034026d84184ee0cc327ee6cc2d6d2ecbd3e9
KBY-TECH <soccer_by@naver.com>
2020-10-24 20:44:58 +0900

    bruteForce total 4 solution

---
+++ src/main/java/bruteForce/Build.java
@@ -0,0 +1,40 @@
+package bruteForce;
+
+import java.util.Scanner;
+
+public class Build {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt(); // 사람 수
+        sc.nextLine();
+        int build[][]=new int[n][n];
+        for (int i = 0; i <n ; i++) {
+            String str[]=sc.nextLine().split(" ");
+                build[i][0]=Integer.parseInt(str[0]);
+                build[i][1]=Integer.parseInt(str[1]);
+        }
+        int ans[]=bodyCheck(build,n);
+        for (int i = 0; i < ans.length; i++) {
+            System.out.print(ans[i]+" ");
+        }
+    }
+    static int[] bodyCheck(int arr[][],int n)
+    {
+        int rank[]=new int[n]; // cnt 처럼 쓰는 배열.
+
+        for (int i = 0; i <n ; i++) {
+            int creteria_kg=arr[i][0];
+            int creteria_cm=arr[i][1];
+            for (int j = 0; j < n; j++) {
+                if(creteria_kg>arr[j][0] && creteria_cm>arr[j][1])
+                    rank[j]++;
+
+            }
+        }
+        for (int i = 0; i <rank.length ; i++) {
+            rank[i]++;
+        }
+
+        return rank;
+    }
+}
---
+++ src/main/java/bruteForce/divideSum.java
@@ -0,0 +1,39 @@
+package bruteForce;
+
+import java.util.Scanner;
+
+public class divideSum {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        // 가장 작은 생성자  구하기
+        // ex 256 -> 245+(2+4+5)
+        int n=sc.nextInt();
+        System.out.println(newSearch(n));
+
+    }
+    public static int newSearch(int n)
+    {
+        int ans=0;
+
+        int sum=0; //생성자 확인용 변수
+
+        for (int i = 0; i <n ; i++) {
+            String str=Integer.toString(i);
+            String s[]=str.split("");
+            // 자리수 꺼내기
+
+            sum=i; // orginal value
+
+            for (int j = 0; j <s.length ; j++) {
+                sum+=Integer.parseInt(s[j]);
+            }
+            if(sum==n)
+            {
+                ans=i;
+                break;
+            }
+            sum=0;
+        }
+        return ans;
+    }
+}
