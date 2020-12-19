0414b8820374527fb52c3cd66d618a59d138d04e
KBY-TECH <soccer_by@naver.com>
2020-12-19 20:20:22 +0900

    12.19

---
+++ src/main/java/Al_Study/기초수학/정수제곱근.java
@@ -0,0 +1,12 @@
+package Al_Study.기초수학;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class 정수제곱근 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+       long n=sc.nextLong();
+        System.out.println((long) Math.ceil(Math.sqrt(n)));
+    }
+}
