e8991931645ac943ac888ff5491badbf608ed624
KBY-TECH <soccer_by@naver.com>
2021-02-02 19:34:07 +0900

    update : RGB , Sensor Solved problems

--- src/main/java/oneDay_twoSol/RealTimeSolving/RGB.java
+++ src/main/java/oneDay_twoSol/RealTimeSolving/RGB.java
@@ -1,25 +1,32 @@
 package oneDay_twoSol.RealTimeSolving;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class RGB {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        int house[][]=new int[n+1][3];
-        for (int i = 1; i < n; i++) {
+        int house[][]=new int[n][3];
+        for (int i = 0; i < n; i++) {
             for (int j = 0; j < 3; j++) {
                 house[i][j]=sc.nextInt();
             }
         }
+        int d[][]=new int[n][3];
+        for (int i = 0; i <3 ; i++) {
+            d[0][i]=house[0][i];
+        }
 
-        int d[][]=new int[n+1][3];
-        for(int i=1;i<n+1;i++){
+        for(int i=1;i<n;i++){
             d[i][0] = Math.min(d[i-1][1], d[i-1][2]) + house[i][0];
             d[i][1] = Math.min(d[i-1][0], d[i-1][2]) + house[i][1];
             d[i][2] = Math.min(d[i-1][0], d[i-1][1]) + house[i][2];
         }
-        System.out.println(Math.min(Math.min(d[n][0],d[n][1]),d[n][2]));
+        for (int i = 0; i < n; i++) {
+            System.out.println(  Arrays.toString(d[i]));
+        }
+        System.out.println(Math.min(Math.min(d[n-1][0],d[n-1][1]),d[n-1][2]));
 
     }
 }
