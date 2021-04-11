cb26610ce7c481091e300e1c2bdd5c28ce525ad2
KBY-TECH <soccer_by@naver.com>
2021-04-12 01:00:05 +0900

    feat : [BaekJoon] Floyed

--- src/main/java/thisCodingTest/ShorthestPath/PS/Floyed.java
+++ src/main/java/thisCodingTest/ShorthestPath/PS/Floyed.java
@@ -8,31 +8,31 @@ import java.util.StringTokenizer;
 
 //https://www.acmicpc.net/problem/11404
 public class Floyed {
-    static final int INF=100_001;
+    static final int INF=(int)1e9;
     public static void main(String[] args) throws IOException {
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         int n=Integer.parseInt(br.readLine());
         int m=Integer.parseInt(br.readLine());
-        int graph[][]=new int[n+1] [n+1];
+        int[][] graph =new int[n+1] [n+1];
         StringTokenizer st;
         for (int i = 1; i < n+1; i++) {
             Arrays.fill(graph[i],INF);
+            graph[i][i]=0;
         }
         for (int i = 0; i < m; i++) {
             st=new StringTokenizer(br.readLine());
             int a=Integer.parseInt(st.nextToken());
             int b=Integer.parseInt(st.nextToken());
-            int w=Integer.parseInt(st.nextToken());
+            int c=Integer.parseInt(st.nextToken());
             if(graph[a][b]!=INF)
-                graph[a][b]=Math.min(graph[a][b],w);
+                graph[a][b]= Math.min(graph[a][b], c);
             else
-                graph[a][b]=w;
+                graph[a][b]=c;
         }
 
         for (int k = 1; k < n+1; k++) {
             for (int i = 1; i < n+1; i++) {
                 for (int j = 1; j < n+1; j++) {
-                    if(i==j) continue;
                     graph[i][j]=Math.min(graph[i][j],graph[i][k]+graph[k][j]);
                 }
             }
