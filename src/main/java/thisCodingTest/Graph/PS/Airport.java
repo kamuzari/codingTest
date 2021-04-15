5e1b1570b5a3175b81b34d4884933955a2b289a3
KBY-TECH <soccer_by@naver.com>
2021-04-15 14:49:13 +0900

    feat : [BaekJoon] Airport

--- src/main/java/thisCodingTest/Graph/PS/Airport.java
+++ src/main/java/thisCodingTest/Graph/PS/Airport.java
@@ -3,8 +3,7 @@ package thisCodingTest.Graph.PS;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.Buffer;
-
+//https://www.acmicpc.net/problem/10775
 public class Airport {
     static int parent[];
     public static void main(String[] args) throws IOException {
