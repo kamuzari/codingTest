000595a31818006f0a33d23541b275150bba07e2
KBY-TECH <soccer_by@naver.com>
2021-04-22 01:23:45 +0900

    feat : [Programmers] StockPrice

---
+++ src/main/java/ProgrammersKit/Stack_Queue/StockPrice.java
@@ -0,0 +1,34 @@
+package ProgrammersKit.Stack_Queue;
+
+import java.util.Stack;
+
+public class StockPrice {
+    public  int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+        Stack<Integer> s = new Stack<Integer>();
+
+        for (int i = 0; i < prices.length; i++) {
+            if (i != prices.length - 1) {
+                for (int j = i + 1; j < prices.length; j++) {
+                    if(prices[i] > prices[j])
+                    {
+                        answer[i] = j - i;
+                        break;
+                    }
+                    else if(j==prices.length-1) // 발견 안되면
+                    {
+                        answer[i] = j - i;
+                        break;
+                    }
+                    else if (prices[i] < prices[j]) {
+                        continue;
+                    }
+                }
+            }
+
+        }
+
+
+        return answer;
+    }
+}
