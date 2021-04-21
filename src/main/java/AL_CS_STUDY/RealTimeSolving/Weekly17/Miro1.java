88e28d72ae80918ec11cf4b20e0be71cd2884f4b
KBY-TECH <soccer_by@naver.com>
2021-04-21 15:56:37 +0900

    update : [BaekJoon] Miro1

--- src/main/java/AL_CS_STUDY/RealTimeSolving/Weekly17/Miro1.java
+++ src/main/java/AL_CS_STUDY/RealTimeSolving/Weekly17/Miro1.java
@@ -1,5 +1,7 @@
 package AL_CS_STUDY.RealTimeSolving.Weekly17;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
@@ -30,13 +32,13 @@ public class Miro1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int T;
-        T = sc.nextInt();
+        T = 10;
 
         StringBuilder sb=new StringBuilder();
         Node start = null;
         Node end = null;
         for (int test_case = 1; test_case <= T; test_case++) {
-            int q=sc.nextInt();
+            sc.nextInt();
             sc.nextLine();
             map = new int[16][16];
             for (int i = 0; i < 16; i++) {
@@ -80,5 +82,80 @@ public class Miro1 {
             }
         }
         return 0;
+    }
+}
+
+class Solution
+{
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+
+    public static void main(String args[]) throws Exception
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = 10;
+
+        for(int test_case = 1; test_case <= T; test_case++)
+        {
+            br.readLine();
+            int[][] map = new int[16][16];
+            int startX = 0;
+            int startY = 0;
+
+            for(int i=0; i<16; i++) {
+                String input = br.readLine();
+
+                for(int j=0; j<16; j++) {
+                    map[i][j] = input.charAt(j) - '0';
+
+                    if(map[i][j]==2) {
+                        startX = i;
+                        startY = j;
+                    }
+                }
+            }
+
+            int ans = bfs(map, startX, startY);
+
+            System.out.println("#"+test_case+" "+ans);
+        }
+    }
+
+    public static int bfs(int[][] map, int x, int y) {
+        Queue<Pair> queue = new LinkedList<>();
+        boolean[][] visited = new boolean[16][16];
+        visited[x][y] = true;
+        queue.add(new Pair(x, y));
+
+        while(!queue.isEmpty()) {
+            Pair temp = queue.poll();
+
+            for(int i=0; i<4; i++) {
+                int nx = temp.x+dx[i];
+                int ny = temp.y+dy[i];
+
+                if(nx<0 || nx>=16 || ny<0 || ny>=16 || visited[nx][ny]) continue;
+
+                if(map[nx][ny]==0) {
+                    visited[nx][ny] = true;
+                    queue.add(new Pair(nx, ny));
+                }
+
+                else if(map[nx][ny]==3)
+                    return 1;
+            }
+        }
+
+        return 0;
+    }
+
+    public static class Pair {
+        int x;
+        int y;
+
+        public Pair(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
     }
 }
