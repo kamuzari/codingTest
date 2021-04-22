966bb87a381d35ee9484d06b2d1a117769bd6be9
KBY-TECH <soccer_by@naver.com>
2021-04-23 00:41:20 +0900

    update : [BaekJoon] SubString

---
+++ src/main/java/tues_thurs_sat/_20210422/SubString.java
@@ -0,0 +1,23 @@
+package tues_thurs_sat._20210422;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class SubString {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        String str=br.readLine();
+        String sub=br.readLine();
+
+        int i = str.indexOf(sub);
+        if(i==-1)
+        {
+            System.out.println(0);
+        }
+        else
+        {
+            System.out.println(1);
+        }
+    }
+}
