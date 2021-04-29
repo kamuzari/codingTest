ff2549f530eae23b0a6a080360d9adbca849e567
KBY-TECH <soccer_by@naver.com>
2021-04-29 20:24:40 +0900

    feat : [Programmers] MaxMin

---
+++ src/main/java/tues_thurs_sat/_20210429/MaxMin.java
@@ -0,0 +1,23 @@
+package tues_thurs_sat._20210429;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class MaxMin {
+    public static void main(String[] args) {
+
+    }
+    public String solution(String s) {
+        String answer = "";
+        String str[]=s.split(" ");
+        ArrayList<Integer> arr=new ArrayList<>();
+        for(int i=0; i<str.length; i++)
+        {
+            arr.add(Integer.parseInt(str[i]));
+        }
+        Collections.sort(arr);
+        answer=String.valueOf(arr.get(0))+" "+String.valueOf(arr.get(1));
+
+        return answer;
+    }
+}
