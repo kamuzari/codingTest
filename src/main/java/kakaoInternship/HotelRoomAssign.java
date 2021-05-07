934e2f406ab75b202e6532053fd1a84a7f0baab8
KBY-TECH <soccer_by@naver.com>
2021-05-08 00:08:23 +0900

    updated : [Programmers] HotelRoomAssign

--- src/main/java/kakaoInternship/HotelRoomAssign.java
+++ src/main/java/kakaoInternship/HotelRoomAssign.java
@@ -9,9 +9,9 @@ public class HotelRoomAssign {
     static Map<Long,Long> map=new LinkedHashMap<>();
     public long[] solution(long k, long[] room_number) {
         long answer[]=new long[room_number.length];
-
+        parent=new long[(int)k+1];
         for (int i = 0; i < room_number.length; i++) {
-            answer[i]=find(room_number[i]);
+            answer[i]=Find(room_number[i]);
         }
         return answer;
     }
@@ -25,5 +25,16 @@ public class HotelRoomAssign {
         long empty=find(next);
         map.put(l,empty);
         return empty;
+    }
+    static long parent[];
+    // 압축기법은 k가 10^12 21억개를 10^8 *21 ==21억 ..까지 가능하다. 하지만 10^12=천억...
+    private long Find(long val)
+    {
+        if(parent[(int)val]==0)
+        {
+            parent[(int)val]=val+1;
+            return val;
+        }
+        return parent[(int)val]=Find(parent[(int)val]);
     }
 }
