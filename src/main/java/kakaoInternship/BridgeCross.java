8be75df76a6b71cbcc27e65edcf352da9eec420f
KBY-TECH <soccer_by@naver.com>
2021-05-08 00:57:30 +0900

    updated : [Programmers] BridgeCross

--- src/main/java/kakaoInternship/BridgeCross.java
+++ src/main/java/kakaoInternship/BridgeCross.java
@@ -4,25 +4,19 @@ public class BridgeCross {
     public static void main(String[] args) {
         int st[] = {2, 4, 5, 3, 2, 1, 4, 2, 5, 1};
         int st2[] = {1,1,1,1,1,1,1,1,1,1,1,1,1,1};
-        int k = 3;
-        System.out.println(solution(st2,k));
+        int st3[] = {1};
+        int k = 1;
+        System.out.println(solution(st,3));
     }
 
     public static int solution(int[] stones, int k) {
         int answer = 0;
-        int max = Integer.MIN_VALUE;
-        int min = Integer.MAX_VALUE;
-        for (int stone : stones) {
-            max = Math.max(max, stone);
-            min = Math.min(min, stone);
-        }
-
-        if(max==min)
-            return min;
+        int max = 200_000_000;
+        int min = 1;
         while (min < max) {
 
             int mid = (max + min) / 2;
-
+            // 4,5 -> mid 4
             if (isCross(stones, k, mid)) {
                 min = mid + 1;
             } else {
