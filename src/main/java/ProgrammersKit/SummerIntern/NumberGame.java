2023b78e4b86b0a2e48272ec690de03c77115af2
KBY-TECH <soccer_by@naver.com>
2021-05-10 20:34:41 +0900

    feat : [Programmers] NumberGame

---
+++ src/main/java/ProgrammersKit/SummerIntern/NumberGame.java
@@ -0,0 +1,26 @@
+package ProgrammersKit.SummerIntern;
+
+import java.util.Arrays;
+
+public class NumberGame {
+    public static void main(String[] args) {
+        int a[]={5, 1, 3, 7};
+        int b[]={2, 2, 6, 8};
+        System.out.println(solution(a,b));
+    }
+    public static int solution(int[] A, int[] B) {
+        int answer = 0;
+        Arrays.sort(A);
+        Arrays.sort(B);
+        int idx=A.length-1;
+        // 어차피 질 거면 제일 안좋은 패.
+        for (int i = A.length-1; i >=0 ; i--) {
+            if(B[idx]>A[i])
+            {
+                answer++;
+                idx--;
+            }
+        }
+        return answer;
+    }
+}
