6df079f529da3606af41ec6e46de979de3d45560
KBY-TECH <soccer_by@naver.com>
2021-05-20 19:24:39 +0900

    feat : [Programmers] NextBigNumber

---
+++ src/main/java/tues_thurs_sat/_20210520/NextBigNumber.java
@@ -0,0 +1,20 @@
+package tues_thurs_sat._20210520;
+
+public class NextBigNumber {
+    public static void main(String[] args) {
+
+    }
+    public int solution(int n) {
+        int answer = 0;
+        int orginalBitCOunt=Integer.bitCount(n);
+        for (int i = n+1;; i++) {
+            int nextBitCount=Integer.bitCount(i);
+            if(orginalBitCOunt==nextBitCount)
+            {
+                answer=i;
+                break;
+            }
+        }
+        return answer;
+    }
+}
