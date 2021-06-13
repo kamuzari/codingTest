7599eec365486c56d1c828c55548895a267ba180
KBY-TECH <soccer_by@naver.com>
2021-06-13 18:50:54 +0900

    Fix : TeenagerShark

--- src/main/java/AL_CS_STUDY/Weekly25/TeenagerShark.java
+++ src/main/java/AL_CS_STUDY/Weekly25/TeenagerShark.java
@@ -1,163 +1,188 @@
 package AL_CS_STUDY.Weekly25;
-import java.util.*;
-import java.io.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class TeenagerShark {
     static class Shark {
-        int x, y, dir, eatSum;
+        int y, x, dir, eatSum;
 
-        Shark() { }
-
-        Shark(int x, int y, int dir, int eatSum) {
-            this.x = x;
+        public Shark(int y, int x, int dir, int eatSum) {
             this.y = y;
+            this.x = x;
             this.dir = dir;
             this.eatSum = eatSum;
         }
     }
 
     static class Fish {
-        int x, y, id, dir;
-        boolean isAlive = true;
+        int idx, y, x, dir;
+        boolean live;
 
-        Fish() { }
-
-        Fish(int x, int y, int id, int dir, boolean isAlive) {
-            this.x = x;
+        public Fish(int idx, int y, int x, int dir, boolean live) {
+            this.idx = idx;
             this.y = y;
-            this.id = id;
+            this.x = x;
             this.dir = dir;
-            this.isAlive = isAlive;
+            this.live = live;
         }
 
-        public Fish(int x, int y, int id, int dir) {
-            this.x = x;
-            this.y = y;
-            this.id = id;
-            this.dir = dir;
+        @Override
+        public String toString() {
+            return "Fish{" +
+                    "idx=" + idx +
+                    ", y=" + y +
+                    ", x=" + x +
+                    ", dir=" + dir +
+                    ", live=" + live +
+                    '}';
         }
     }
 
-    // 0 부터 7 까지 순서대로 ↑, ↖, ←, ↙, ↓, ↘, →, ↗
-    static int[] dx = {-1, -1, 0, 1, 1, 1, 0, -1};
-    static int[] dy = {0, -1, -1, -1, 0, 1, 1, 1};
+    static int[] dy = {-1, -1, 0, 1, 1, 1, 0, -1};
+    static int[] dx = {0, -1, -1, -1, 0, 1, 1, 1};
     static int maxSum = 0;
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        int[][] arr = new int[4][4];
-        List<Fish> fishes = new ArrayList<>();
+        int map[][] = new int[4][4];
+        List<Fish> fishList = new LinkedList<>();
 
-        // input
         for (int i = 0; i < 4; i++) {
             st = new StringTokenizer(br.readLine());
 
             for (int j = 0; j < 4; j++) {
-                Fish f = new Fish(i,j,Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken()));
-                fishes.add(f);
-                arr[i][j] = f.id;
+                int idx = Integer.parseInt(st.nextToken());
+                int dir = Integer.parseInt(st.nextToken())-1;
+
+                fishList.add(new Fish(idx, i, j, dir, true));
+                map[i][j] = idx;
             }
         }
 
-        // 물고기는 작은 순서부터 이동해야 하기 때문에 미리 정렬해둠
-        Collections.sort(fishes, new Comparator<Fish>() {
-            @Override
-            public int compare(Fish o1, Fish o2) {
-                return o1.id - o2.id;
-            }
-        });
+        fishList.sort((o1, o2) -> o1.idx - o2.idx);
+//        System.out.println(fishList);
+        Fish eating = fishList.get(map[0][0] - 1);
+        Shark shark = new Shark(0, 0, eating.dir, eating.idx);
+        eating.live = false;
+        map[0][0] = -1;
 
-        // 상어는 (0, 0) 물고기를 먹고 시작하며 위치는 -1 로 표현
-        Fish f = fishes.get(arr[0][0] - 1);
-        Shark shark = new Shark(0, 0, f.dir, f.id);
-        f.isAlive = false;
-        arr[0][0] = -1;
-
-        // solution
-        dfs(arr, shark, fishes);
+        dfs(shark, map, fishList);
         System.out.println(maxSum);
     }
 
-    // 재귀로 상어가 이동할 수 없을 때까지 반복한다.
-    static void dfs(int[][] arr, Shark shark, List<Fish> fishes) {
-        // 잡아먹은 양의 최대값 구하기
-        if (maxSum < shark.eatSum) {
-            maxSum = shark.eatSum;
-        }
+    private static void dfs(Shark shark, int[][] map, List<Fish> fishList) {
 
-        // 모든 물고기 순서대로 이동
-        fishes.forEach(e -> moveFish(e, arr, fishes));
+        maxSum = Math.max(maxSum, shark.eatSum);
+
+        fishList.forEach(fish -> move(fish, map, fishList));
 
         for (int dist = 1; dist < 4; dist++) {
-            int nx = shark.x + dx[shark.dir] * dist;
             int ny = shark.y + dy[shark.dir] * dist;
+            int nx = shark.x + dx[shark.dir] * dist;
 
-            if (0 <= nx && nx < 4 && 0 <= ny && ny < 4 && arr[nx][ny] > 0) {
-                // 물고기 잡아먹고 dfs
-                int[][] arrCopies = copyArr(arr);
-                List<Fish> fishCopies = copyList(fishes);
+            if (ny >= 0 && nx >= 0 && ny < 4 && nx < 4 && map[ny][nx] > 0) {
+                int copy[][] = copyArr(map);
+                List<Fish> copyList = copyList(fishList);
 
-                arrCopies[shark.x][shark.y] = 0;
-                Fish f = fishCopies.get(arr[nx][ny] - 1);
-                Shark newShark = new Shark(f.x, f.y, f.dir, shark.eatSum + f.id);
-                f.isAlive = false;
-                arrCopies[f.x][f.y] = -1;
+                copy[shark.y][shark.x] = 0;
+                Fish eating = copyList.get(map[ny][nx] - 1);
+                Shark newShark = new Shark(eating.y, eating.x, eating.dir, shark.eatSum + eating.idx);
+                eating.live = false;
+                copy[eating.y][eating.x] = -1;
+//                for (int i = 0; i < 4; i++) {
+//                    System.out.println(Arrays.toString(copy[i]));
+//                }
 
-                dfs(arrCopies, newShark, fishCopies);
+                dfs(newShark, copy, copyList);
             }
         }
     }
 
-    // 이동가능한 칸은 빈칸, 다른 물고기가 있는 칸
-    // 45도 반시계 방향으로 회전하면서 스캔
-    static void moveFish(Fish fish, int[][] arr, List<Fish> fishes) {
-        if (fish.isAlive == false) return;
+    private static int[][] copyArr(int[][] map) {
+        int temp[][] = new int[4][4];
+        for (int i = 0; i < 4; i++) {
+            temp[i] = map[i].clone();
+        }
 
+        return temp;
+    }
+
+    private static List<Fish> copyList(List<Fish> fishList) {
+        List<Fish> temp = new LinkedList<>();
+        fishList.forEach(fish -> temp.add(new Fish(fish.idx, fish.y, fish.x, fish.dir, fish.live)));
+        return temp;
+    }
+
+    private static void move(Fish fish, int[][] arr, List<Fish> fishList) {
+        if (!fish.live) {
+            return;
+        }
         for (int i = 0; i < 8; i++) {
-            int nextDir = (fish.dir + i) % 8;
-            int nx = fish.x + dx[nextDir];
-            int ny = fish.y + dy[nextDir];
+            int nDir = (fish.dir + i) % 8;
+            int ny = fish.y + dy[nDir];
+            int nx = fish.x + dx[nDir];
 
-            if (0 <= nx && nx < 4 && 0 <= ny && ny < 4 && arr[nx][ny] > -1) {
-                arr[fish.x][fish.y] = 0;
-
-                if (arr[nx][ny] == 0) {
-                    fish.x = nx;
+            if (ny >= 0 && nx >= 0 && ny < 4 && nx < 4 && arr[ny][nx] > -1) {
+                arr[fish.y][fish.x] = 0;
+                if (arr[ny][nx] == 0) {
                     fish.y = ny;
-                } else {
-                    Fish temp = fishes.get(arr[nx][ny] - 1);
-                    temp.x = fish.x;
-                    temp.y = fish.y;
-                    arr[fish.x][fish.y] = temp.id;
-
                     fish.x = nx;
+                } else if (arr[ny][nx] != 0) {
+                    Fish swapping = fishList.get(arr[ny][nx] - 1);
+                    swapping.y = fish.y;
+                    swapping.x = fish.x;
+                    arr[fish.y][fish.x] = swapping.idx;
+
                     fish.y = ny;
+                    fish.x = nx;
                 }
-
-                arr[nx][ny] = fish.id;
-                fish.dir = nextDir;
+                arr[ny][nx] = fish.idx;
+                fish.dir = nDir;
                 return;
             }
         }
     }
 
-    static int[][] copyArr(int[][] arr) {
-        int[][] temp = new int[4][4];
-
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
-                temp[i][j] = arr[i][j];
-            }
-        }
-
-        return temp;
-    }
-
-    static List<Fish> copyList(List<Fish> fishes) {
-        List<Fish> temp = new ArrayList<>();
-        fishes.forEach(e -> temp.add(new Fish(e.x, e.y, e.id, e.dir, e.isAlive)));
-        return temp;
-    }
 }
+/*
+
+[Fish{x=1, y=1, id=1, dir=7, isAlive=true},
+  Fish{x=0, y=1, id=2, dir=2, isAlive=true},
+    Fish{x=1, y=0, id=3, dir=0, isAlive=true},
+      Fish{x=2, y=2, id=4, dir=2, isAlive=true},
+        Fish{x=3, y=2, id=5, dir=1, isAlive=true}, Fish{x=2, y=0, id=6, dir=0, isAlive=true}, Fish{x=0, y=0, id=7, dir=5, isAlive=true}, Fish{x=3, y=1, id=8, dir=6, isAlive=true}, Fish{x=0, y=3, id=9, dir=7, isAlive=true}, Fish{x=1, y=3, id=10, dir=0, isAlive=true}, Fish{x=2, y=3, id=11, dir=3, isAlive=true}, Fish{x=3, y=3, id=12, dir=1, isAlive=true}, Fish{x=2, y=1, id=13, dir=5, isAlive=true}, Fish{x=1, y=2, id=14, dir=6, isAlive=true}, Fish{x=0, y=2, id=15, dir=5, isAlive=true}, Fish{x=3, y=0, id=16, dir=0, isAlive=true}]
+0
+[0, 2, 9, 10]
+[6, -1, 1, 14]
+[16, 5, 15, 13]
+[3, 4, 11, 8]
+7
+[-1, 9, 10, 14]
+[16, 0, 0, 1]
+[5, 2, 13, 8]
+[3, 4, 11, 15]
+19
+[0, 2, 9, 10]
+[6, 12, 1, 14]
+[16, 5, -1, 13]
+[3, 4, 11, 8]
+[12, 9, 10, 14]
+[16, 6, 0, 1]
+[5, 13, 0, 8]
+[3, 4, 2, -1]
+25
+[0, 2, 9, 10]
+[6, 12, 1, 14]
+[16, 5, 15, 13]
+[3, 4, 11, -1]
+33
+
+
+*/
