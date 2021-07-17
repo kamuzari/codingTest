52b78a6ba3a562a358fd930921a7e12195cf02a6
KBY-TECH <soccer_by@naver.com>
2021-07-17 19:16:02 +0900

    feat : [SWEA] INFdictionary, Monster, ThreeDimensionFarmer

---
+++ src/main/java/AL_CS_STUDY/RealTimeSolving/Weekly29/INFdictionary.java
@@ -0,0 +1,22 @@
+package AL_CS_STUDY.RealTimeSolving.Weekly29;
+
+import java.io.*;
+public class INFdictionary {
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T=Integer.parseInt(br.readLine());
+        StringBuilder sb=new StringBuilder();
+        for (int testCase = 1; testCase <= T; testCase++) {
+            String str1=br.readLine();
+            String str2=br.readLine();
+            String answer="";
+           if(!(str1+"a").equals(str2))
+                answer="Y";
+           else
+               answer="N";
+
+            sb.append("#"+testCase).append(" "+answer).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
---
+++ src/main/java/AL_CS_STUDY/RealTimeSolving/Weekly29/Monster.java
@@ -0,0 +1,30 @@
+package AL_CS_STUDY.RealTimeSolving.Weekly29;
+
+import java.util.*;
+import java.io.*;
+public class Monster {
+    public static int D, L, N;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder("");
+        StringTokenizer st;
+
+        int T = Integer.parseInt(br.readLine());
+        for (int tc = 1; tc <= T; ++tc) {
+            st = new StringTokenizer(br.readLine());
+            D = Integer.parseInt(st.nextToken());
+            L = Integer.parseInt(st.nextToken());
+            N = Integer.parseInt(st.nextToken());
+
+            int damage = 0;
+            double percentage = L * 0.01;
+            for (int i = 0; i < N; ++i) {
+                damage += D * (1 + (i * (percentage)));
+            }
+
+            sb.append("#").append(tc).append(" ").append(damage).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
