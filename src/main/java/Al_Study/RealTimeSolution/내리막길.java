637457325aea95c841f06db2aa678857120b2753
KBY-TECH <soccer_by@naver.com>
2021-08-02 00:00:05 +0900

    Remove : CodingTest dir rm

--- src/main/java/Al_Study/RealTimeSolution/내리막길.java
+++
@@ -1,7 +0,0 @@
-package Al_Study.RealTimeSolution;
-
-public class 내리막길 {
-    public static void main(String[] args) {
-        
-    }
-}
--- src/main/java/Al_Study/RealTimeSolution/카드게임.java
+++
@@ -1,4 +0,0 @@
-package Al_Study.RealTimeSolution;
-
-public class 카드게임 {
-}
