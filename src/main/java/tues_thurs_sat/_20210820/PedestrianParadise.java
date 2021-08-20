5a779a93ff1c2caacec110b570d0fb3c44885d7e
KBY-TECH <soccer_by@naver.com>
2021-08-21 00:33:35 +0900

    style : [programmers] PedestrianParadise

--- src/main/java/tues_thurs_sat/_20210820/PedestrianParadise.java
+++ src/main/java/tues_thurs_sat/_20210820/PedestrianParadise.java
@@ -4,11 +4,11 @@ public class PedestrianParadise {
     static final int MOD = 20170805;
     public int solution(int m, int n, int[][] cityMap) {
         int answer = 0;
-        int[][] rMap=new int[m+1][n+1];
-        int[][] uMap=new int[m+1][n+1];
+        int[][] fromUp=new int[m+1][n+1];
+        int[][] fromRight=new int[m+1][n+1];
 
-        rMap[1][1]=1;
-        uMap[1][1]=1;
+        fromUp[1][1]=1;
+        fromRight[1][1]=1;
 
         for(int i=1; i<=m; i++)
         {
@@ -17,8 +17,8 @@ public class PedestrianParadise {
                 int condition=cityMap[i-1][j-1];
                 if(condition==0)
                 {
-                    rMap[i][j]+=(rMap[i-1][j]+uMap[i][j-1])%MOD;
-                    uMap[i][j]+=(rMap[i-1][j]+uMap[i][j-1])%MOD;
+                    fromUp[i][j]+=(fromUp[i-1][j]+fromRight[i][j-1])%MOD;
+                    fromRight[i][j]+=(fromUp[i-1][j]+fromRight[i][j-1])%MOD;
                 }
                 else if(condition==1)
                 {
@@ -26,12 +26,12 @@ public class PedestrianParadise {
                 }
                 else if(condition==2)
                 {
-                    rMap[i][j]=rMap[i-1][j];
-                    uMap[i][j]=uMap[i][j-1];
+                    fromUp[i][j]=fromUp[i-1][j];
+                    fromRight[i][j]=fromRight[i][j-1];
                 }
 
             }
         }
-        return (rMap[m-1][n]+uMap[m][n-1])%MOD;
+        return (fromUp[m-1][n]+fromRight[m][n-1])%MOD;
     }
 }
