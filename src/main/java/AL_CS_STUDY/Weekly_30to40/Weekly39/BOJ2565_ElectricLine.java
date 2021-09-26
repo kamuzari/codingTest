480de1c6ce14f2622000d88595b92b491fe2be41
KBY-TECH <soccer_by@naver.com>
2021-09-26 22:25:22 +0900

    feat : [BaekJoon] BOJ2565_ElectricLine

--- src/main/java/AL_CS_STUDY/Weekly_30to40/Weekly39/BOJ2565_ElectricLine.java
+++ src/main/java/AL_CS_STUDY/Weekly_30to40/Weekly39/BOJ2565_ElectricLine.java
@@ -7,31 +7,31 @@ import java.util.StringTokenizer;
 
 public class BOJ2565_ElectricLine {
     public static void main(String[] args) throws IOException {
-        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
-        int n=Integer.parseInt(br.readLine());
-        final StringTokenizer st = new StringTokenizer(br.readLine());
-        int arr[][]=new int[n][2];
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int arr[][] = new int[n][2];
         for (int i = 0; i < n; i++) {
-            arr[i][0]=Integer.parseInt(st.nextToken());
-            arr[i][1]=Integer.parseInt(st.nextToken());
+            final StringTokenizer st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
         }
         Arrays.sort(arr, new Comparator<int[]>() {
             @Override
             public int compare(int[] a, int[] b) {
-                return a[0]-b[0];
+                return a[0] - b[0];
             }
         });
 
-        int dp[]=new int[n+1];
-        int max=0;
-        for (int i = 1; i <n+1 ; i++) {
-            dp[i]=1;
+        int dp[] = new int[n + 1];
+        int max = 0;
+        for (int i = 1; i < n; i++) {
+            dp[i] = 1;
             for (int j = 0; j < i; j++) {
-                if(arr[i][1]>arr[j][1])
-                    dp[i]=Math.max(dp[i],dp[j]+1);
+                if (arr[i][1] > arr[j][1])
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
             }
-            max=Math.max(max,dp[i]);
+            max = Math.max(max, dp[i]);
         }
-        System.out.println(n-max);
+        System.out.println(n - max);
     }
 }
