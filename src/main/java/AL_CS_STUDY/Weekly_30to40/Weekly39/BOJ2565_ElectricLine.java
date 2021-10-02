d660f13fb7e3eeefd613bac1072ef149cb4d734a
KBY-TECH <soccer_by@naver.com>
2021-10-02 20:38:41 +0900

    feat : [BaekJoon] BOJ2565_ElectricLine

--- src/main/java/AL_CS_STUDY/Weekly_30to40/Weekly39/BOJ2565_ElectricLine.java
+++ src/main/java/AL_CS_STUDY/Weekly_30to40/Weekly39/BOJ2565_ElectricLine.java
@@ -22,10 +22,10 @@ public class BOJ2565_ElectricLine {
             }
         });
 
-        int dp[] = new int[n + 1];
+        int dp[] = new int[n];
+        Arrays.fill(dp,1);
         int max = 0;
-        for (int i = 1; i < n; i++) {
-            dp[i] = 1;
+        for (int i = 0; i < n; i++) {
             for (int j = 0; j < i; j++) {
                 if (arr[i][1] > arr[j][1])
                     dp[i] = Math.max(dp[i], dp[j] + 1);
