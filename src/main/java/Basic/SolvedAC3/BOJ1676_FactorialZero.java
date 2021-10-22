7ee69fb1afdae7e07f0b6dbfff2d66fd679940cd
KBY-TECH <soccer_by@naver.com>
2021-10-22 19:15:07 +0900

    feat : [Baekjoon] BOJ1676_FactorialZero

---
+++ src/main/java/Basic/SolvedAC3/BOJ1676_FactorialZero.java
@@ -0,0 +1,16 @@
+package Basic.SolvedAC3;
+
+import java.io.*;
+public class BOJ1676_FactorialZero {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n=Integer.parseInt(br.readLine());
+
+        int cnt=0;
+        while (n>=5){
+            cnt+=n/5;
+            n/=5;
+        }
+        System.out.println(cnt);
+    }
+}
