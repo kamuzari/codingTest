77fc9f3f94d596226e08f739787f7f6357c62675
KBY-TECH <soccer_by@naver.com>
2021-11-03 18:50:07 +0900

    feat : [Baekjoon] BOJ9461_PadovanSequenceNumber

---
+++ src/main/java/Basic/SolvedAC3/BOJ9461_PadovanSequenceNumber.java
@@ -0,0 +1,23 @@
+package Basic.SolvedAC3;
+import java.io.*;
+
+public class BOJ9461_PadovanSequenceNumber {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder answers=new StringBuilder();
+        int testCase=Integer.parseInt(br.readLine());
+        while (testCase-->0){
+            long arr[]=new long[101];
+            arr[1]=1;
+            arr[2]=1;
+            arr[3]=1;
+            int n=Integer.parseInt(br.readLine());
+            for (int i = 4; i <=n ; i++) {
+                arr[i]=arr[i-2]+arr[i-3];
+            }
+            answers.append(arr[n]).append("\n");
+        }
+        System.out.println(answers);
+
+    }
+}
