10bd232085078efacac7adc7013fcd6fe9961056
KBY-TECH <soccer_by@naver.com>
2021-11-04 19:44:25 +0900

    feat : BOJ11279_MaxHeap

---
+++ src/main/java/Basic/SolvedAC3/BOJ11279_MaxHeap.java
@@ -0,0 +1,26 @@
+package Basic.SolvedAC3;
+
+import java.io.*;
+import java.util.PriorityQueue;
+
+public class BOJ11279_MaxHeap {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) -> o2 - o1);
+        StringBuilder answers = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            int input = Integer.parseInt(br.readLine());
+            if (input == 0) {
+                if (pq.isEmpty()) {
+                    answers.append(0 + "\n");
+                } else {
+                    answers.append(pq.poll() + "\n");
+                }
+            }else{
+                pq.offer(input);
+            }
+        }
+        System.out.println(answers);
+    }
+}
