1b63a8c2c04f410ec708eca46e7a3bcb9df9c035
KBY-TECH <soccer_by@naver.com>
2021-11-05 19:41:41 +0900

    feat : [Programmers] NotationTransmission

---
+++ src/main/java/Programmers/LV2/NotationTransmission.java
@@ -0,0 +1,21 @@
+package Programmers.LV2;
+
+public class NotationTransmission {
+    public static void main(String[] args) {
+        NotationTransmission p = new NotationTransmission();
+        System.out.println(p.conversion(8,2));
+    }
+
+    public String conversion(int number,int notation){
+        StringBuilder answer=new StringBuilder();
+        while (number>0){
+            if(number%notation<10){
+                answer.append(number%notation);
+            }else{
+                answer.append((char)number%notation-10+'A');
+            }
+            number/=notation;
+        }
+        return answer.reverse().toString();
+    }
+}
