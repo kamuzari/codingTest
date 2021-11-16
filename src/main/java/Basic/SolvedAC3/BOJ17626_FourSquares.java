b88fcae43f12ef90b9d1cf75d7f30095fd57298d
KBY-TECH <soccer_by@naver.com>
2021-11-16 22:07:31 +0900

    feat : [Baek] BOJ17626_FourSquares

---
+++ src/main/java/Basic/SolvedAC3/BOJ17626_FourSquares.java
@@ -0,0 +1,23 @@
+package Basic.SolvedAC3;
+
+import java.io.*;
+import java.util.Arrays;
+
+public class BOJ17626_FourSquares {
+    static final int INF = (int) 1e9;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int dp[] = new int[n + 1];
+        Arrays.fill(dp, INF);
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j * j <= i; j++) {
+                dp[i] = Math.min(dp[i - j * j] + 1, dp[i]);
+            }
+        }
+        System.out.println(dp[n]);
+    }
+}
