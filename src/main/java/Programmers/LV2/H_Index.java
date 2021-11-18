518638bd599a29df85a89282498993f9a4b3af88
KBY-TECH <soccer_by@naver.com>
2021-11-18 16:03:52 +0900

    feat : [Programmers] H_Index

---
+++ src/main/java/Programmers/LV2/H_Index.java
@@ -0,0 +1,18 @@
+package Programmers.LV2;
+public class H_Index {
+    public int solution(int[] citations) {
+        int answer = 0;
+        for(int i=1000; i>=0 ; i--){
+            int cnt=0;
+            for(int val :citations){
+                if(val>=i){
+                    cnt++;
+                }
+            }
+            if(cnt>=i){
+                return i;
+            }
+        }
+        return answer;
+    }
+}
