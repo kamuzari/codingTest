774c8abcd4522c8f0101f2579889fc47d9a9316f
KBY-TECH <soccer_by@naver.com>
2021-11-19 18:38:31 +0900

    feat : [BaekJoon] BOJ1629_Multiple

---
+++ src/main/java/Basic/SolvedAC4/BOJ1629_Multiple.java
@@ -0,0 +1,29 @@
+package Basic.SolvedAC4;
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class BOJ1629_Multiple {
+
+    private static int c;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st=new StringTokenizer(br.readLine());
+        int a=Integer.parseInt(st.nextToken());
+        int b=Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken());
+        System.out.println(pow(a,b));
+
+    }
+//    base := 밑, exponenet := 지수
+    public static long pow(long a,int exponenet){
+        if(exponenet==1){
+            return a%c;
+        }
+        long tmp=pow(a,exponenet/2);
+        if(exponenet%2==1){
+            return (tmp*tmp%c)*a%c;
+        }
+        return tmp*tmp%c;
+    }
+}
