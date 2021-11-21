7c276accdbf2b32ee7bc884390a5168b27d9b53d
KBY-TECH <soccer_by@naver.com>
2021-11-21 17:03:15 +0900

    feat : [Programmers] EnglishWord_endToEnd

---
+++ src/main/java/Programmers/EnglishWord_endToEnd.java
@@ -0,0 +1,21 @@
+package Programmers;
+import java.util.*;
+public class EnglishWord_endToEnd {
+    Set<String> wordSet=new HashSet<>();
+    public int[] solution(int n, String[] words) {
+        String prev="";
+        int loop=0;
+        for(int i=0; i<words.length; i++){
+            boolean nextPossible=true;
+            if(!prev.equals("")){
+                char prevCh=prev.charAt(prev.length()-1);
+                if(prevCh!=words[i].charAt(0) || wordSet.contains(words[i])) {
+                    return new int[]{i%n+1,i/n+1};
+                }
+            }
+            prev=words[i];
+            wordSet.add(words[i]);
+        }
+        return new int[]{0,0};
+    }
+}
