bc30c8c8dec666247aa5650ee70e9da6857449fd
whyWhale <67587446+whyWhale@users.noreply.github.com>
2021-11-29 19:09:09 +0900

    202111-4주차-beakjoon , programmers (#2)
    * feat : [baekjoon] BOJ2096_GoDown
    
    * feat : [baekjoon] BOJ2206_WallBreakMove
    
    * feat : [programmers] SkillTree
    
    * feat : [programmers] NumberingAfterQuadCompression
    
    * feat : [programmers] SkillTree
    
    * feat : [programmers] VisitedLength
    
    * feat : [programmers] JustNowThatSong
    
    * feat : [programmers] BigSquareSearch

---
+++ src/main/java/Programmers/LV2/NumberingAfterQuadCompression.java
@@ -0,0 +1,50 @@
+package Programmers.LV2;
+
+public class NumberingAfterQuadCompression {
+    static final int ZERO=0;
+    static final int ONE=1;
+    int answer[]=new int[2];
+    int map[][];
+    public int[] solution(int[][] arr) {
+        int n=arr.length;
+        map=arr;
+        recursion(0,0,n);
+        return answer;
+    }
+    public void recursion(int sy,int sx,int n){
+        if(n==1){
+            int val=map[sy][sx];
+            if(val==ZERO){
+                answer[ZERO]++;
+            }else if(val==ONE){
+                answer[ONE]++;
+            }
+            return;
+        }
+        if(allElementsEqual(sy,sx,n)){
+            int val=map[sy][sx];
+            if(val==ZERO){
+                answer[ZERO]++;
+            }else if(val==ONE){
+                answer[ONE]++;
+            }
+            return;
+        }
+        int len=n/2;
+        recursion(sy,sx,len);
+        recursion(sy,sx+len,len);
+        recursion(sy+len,sx,len);
+        recursion(sy+len,sx+len,len);
+    }
+    public boolean allElementsEqual(int sy,int sx,int len){
+        int standard=map[sy][sx];
+        for(int i=sy; i<sy+len; i++){
+            for(int j=sx; j<sx+len; j++){
+                if(standard!=map[i][j]){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
