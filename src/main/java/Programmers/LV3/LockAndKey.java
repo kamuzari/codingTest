b1e8a49e2fd3868426bd0eb75454dc8c716227a6
beyongyeon.kim <byeongyeon.kim@webtoonscorp.com>
2022-01-12 23:46:39 +0900

    feat: LockAndKey

---
+++ src/main/java/Programmers/LV3/LockAndKey.java
@@ -0,0 +1,61 @@
+package Programmers.LV3;
+
+public class LockAndKey {
+    static final int DIR = 4;
+    static int lockLen, keyLen;
+
+    public boolean solution(int[][] key, int[][] lock) {
+        lockLen = lock.length;
+        keyLen = key.length;
+        int sizeUpLock[][] = new int[lockLen * 3][lockLen * 3];
+        for (int i = 0; i < lockLen; i++) {
+            for (int j = 0; j < lockLen; j++) {
+                sizeUpLock[lockLen + i][lockLen + j] = lock[i][j];
+            }
+        }
+        for (int dir = 0; dir < DIR; dir++) {
+            key = rotateKey(key);
+            for (int startRow = 0; startRow < lockLen * 2; startRow++) {
+                for (int startCol = 0; startCol < lockLen * 2; startCol++) {
+                    // 덧칠하기
+                    for (int i = 0; i < keyLen; i++) {
+                        for (int j = 0; j < keyLen; j++) {
+                            sizeUpLock[startRow + i][startCol + j] += key[i][j];
+                        }
+                    }
+                    if (isAllOne(sizeUpLock)) {
+                        return true;
+                    }
+                    // 지우기
+                    for (int i = 0; i < keyLen; i++) {
+                        for (int j = 0; j < keyLen; j++) {
+                            sizeUpLock[startRow + i][startCol + j] -= key[i][j];
+                        }
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    public int[][] rotateKey(int[][] key) {
+        int temp[][] = new int[keyLen][keyLen];
+        for (int i = 0; i < keyLen; i++) {
+            for (int j = 0; j < keyLen; j++) {
+                temp[i][j] = key[keyLen - 1 - j][i];
+            }
+        }
+        return temp;
+    }
+
+    public boolean isAllOne(int[][] sizeUpLock) {
+        for (int i = lockLen; i < lockLen * 2; i++) {
+            for (int j = lockLen; j < lockLen * 2; j++) {
+                if (sizeUpLock[i][j] != 1) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
