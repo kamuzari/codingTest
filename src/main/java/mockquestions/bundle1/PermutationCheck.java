1ac8d865682eab5c9b15c04bf38a73342fb50194
whyWhale <67587446+whyWhale@users.noreply.github.com>
2022-08-25 23:45:02 +0900

    programmers 모음집 1회 (#37)
    https://www.notion.so/bylog/1-470cca0bab414a5c93875b9e67408733 완료

---
+++ src/main/java/mockquestions/bundle1/PermutationCheck.java
@@ -0,0 +1,20 @@
+package mockquestions.bundle1;
+
+import java.util.Arrays;
+
+public class PermutationCheck {
+	/**
+	 * note : https://www.notion.so/bylog/1-470cca0bab414a5c93875b9e67408733
+	 */
+	public boolean solution(int[] arr) {
+		Arrays.sort(arr);
+
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] != i + 1) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+}
