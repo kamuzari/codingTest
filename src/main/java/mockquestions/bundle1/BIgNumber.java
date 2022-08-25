0730ae67dc1091e3615bc9e9a1f1ba5e3d8f440b
킹받드라슈 <soccer_by@naver.com>
2022-08-25 23:42:45 +0900

    programmers: 가장 큰수(lv2)

---
+++ src/main/java/mockquestions/bundle1/BIgNumber.java
@@ -0,0 +1,30 @@
+package mockquestions.bundle1;
+
+import java.util.Stack;
+
+public class BIgNumber {
+	public static String solution(String number, int k) {
+		Stack<Integer> s = new Stack<>();
+		int n=number.length();
+
+		for (int i = 0; i < n; i++) {
+			int ch = number.charAt(i)-'0';
+			while (!s.isEmpty() && s.peek() < ch && k > 0) {
+				s.pop();
+				k--;
+			}
+			s.push(ch);
+		}
+		StringBuilder answer=new StringBuilder();
+
+		while(!s.isEmpty()){
+			if(k>0) {
+				k--;
+				s.pop(); continue;
+			}
+			answer.append(s.pop());
+		}
+
+		return answer.reverse().toString();
+	}
+}
