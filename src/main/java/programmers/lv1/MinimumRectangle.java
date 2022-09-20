128b16294df665eb9bf6cfd63e12e999f6937ae5
킹받드라슈 <soccer_by@naver.com>
2022-09-20 15:01:55 +0900

    programmers: 최소 직사각형(lv1) 최적화

--- src/main/java/programmers/lv1/MinimumRectangle.java
+++ src/main/java/programmers/lv1/MinimumRectangle.java
@@ -6,35 +6,50 @@ public class MinimumRectangle {
 	}
 
 	/**
+	 * note : n * 2 번 loop 최적화 -> n * 1번의 loop
+	 * @param sizes
+	 * @return
+	 */
+	public int solutionOptimize(int[][] sizes) {
+		int maxRow = 0;
+		int maxCol = 0;
+
+		for (int[] card : sizes) {
+			maxRow = Math.max(maxRow, Math.max(card[0], card[1]));
+			maxCol = Math.max(maxCol, Math.min(card[0], card[1]));
+		}
+
+		return maxRow * maxCol;
+	}
+
+	/**
 	 note : 첫번째 풀이
-	  - 한쪽을 큰 값으로 셋팅 후 -> 다른 한쪽 중 큰 값을 탐색함.
+	 - 한쪽을 큰 값으로 셋팅 후 -> 다른 한쪽 중 큰 값을 탐색함.
 	 hint : 한쪽 (가로, 세로 중 택 1)을 가장 큰 값으로 몰아넣기!
 	 */
 	public int solution(int[][] sizes) {
-		int maxRow=0;
-		int maxIdx=0;
+		int maxRow = 0;
 
-		for(int i=0; i<sizes.length; i++){
-			int row=sizes[i][0];
-			int col=sizes[i][1];
+		for (int i = 0; i < sizes.length; i++) {
+			int row = sizes[i][0];
+			int col = sizes[i][1];
 
-			if(row<col){
-				sizes[i][0]=col;
-				sizes[i][1]=row;
+			if (row < col) {
+				sizes[i][0] = col;
+				sizes[i][1] = row;
 			}
 
-
-			maxRow=Math.max(maxRow,sizes[i][0]);
+			maxRow = Math.max(maxRow, sizes[i][0]);
 		}
 
-		int maxCol=0;
+		int maxCol = 0;
 
-		for(int i=0; i<sizes.length; i++){
-			int col=sizes[i][1];
+		for (int i = 0; i < sizes.length; i++) {
+			int col = sizes[i][1];
 
-			maxCol=Math.max(maxCol,col);
+			maxCol = Math.max(maxCol, col);
 		}
 
-		return maxRow*maxCol;
+		return maxRow * maxCol;
 	}
 }
