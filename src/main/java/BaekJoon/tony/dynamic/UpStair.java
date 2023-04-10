bdd6cd76df4ab46ac52bf713ba9cdc0c2e022436
킹받드라슈 <soccer_by@naver.com>
2023-04-11 04:44:08 +0900

    feat(baekjoon): 계단오르기

---
+++ src/main/java/BaekJoon/tony/dynamic/UpStair.java
@@ -0,0 +1,29 @@
+package BaekJoon.tony.dynamic;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class UpStair {
+	public static void main(String[] args) throws IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+		int n = Integer.parseInt(reader.readLine());
+		int stairs[] = new int[n + 1];
+		for (int i = 1; i <= n; i++) {
+			stairs[i] = Integer.parseInt(reader.readLine());
+		}
+
+		int dp[] = new int[n + 1];
+		dp[1] = stairs[1];
+
+		if (n >= 2)
+			dp[2] += stairs[1] + stairs[2];
+
+		for (int i = 3; i <= n; i++) {
+			dp[i] = Math.max(dp[i - 2], dp[i - 3]+stairs[i-1]) + stairs[i];
+		}
+
+		System.out.println(dp[n]);
+	}
+}
