1b5a9b1e2f9502a2f86c44f57bf1ce140575b565
whyWhale <soccer_by@naver.com>
2024-03-26 08:56:28 +0900

    feat(baekjoon): mootube

--- src/main/java/BaekJoon/honeytip/mootube/MooTube.java
+++ src/main/java/BaekJoon/honeytip/mootube/MooTube.java
@@ -1,4 +1,4 @@
-package BaekJoon.honeytip;
+package BaekJoon.honeytip.mootube;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -44,16 +44,27 @@ public class MooTube {
 			int videoCount = 0;
 			LinkedList<Integer> q = new LinkedList<>();
 			boolean[] v = new boolean[n + 1];
-			v[start] = true;
 			q.offer(start);
+			v[start] = true;
 			while (!q.isEmpty()) {
 				int cur = q.poll();
+
 				for (Node next : adjacency[cur]) {
-					if (!v[next.v] && next.cost >= graterThanValue) {
-						v[next.v] = true;
-						videoCount++;
-						q.offer(next.v);
-					}
+					/**
+					 * review: 한 시박점으로 부터 퍼져나가는 BFS 성질을 이해하면 됨
+					 *   - 다음 정점으로 가기 까지 K미만의 유사도를 가진 경로를 거치게 되면 다음 노드에서
+					 *     다음 노드 에 K보다 크다고 해도 이전 K미만의 유사도 경로 값으로 대치될 것이기 때문에
+					 *     애초에 다음 노드로 가는것이 k보다 작으면 가지 않는다.
+					 *     문제 조건을 보면 임의의 두 쌍 사이의 동영상이라고 언급한 바 있다. 그리고 힌트를 보면 이전 노드로 부터
+					 *     온 경로값의 값과 비교하여 최솟값으로 갱신한다.
+					 */
+					if (v[next.v])
+						continue;
+					if (next.cost < graterThanValue)
+						continue;
+					videoCount++;
+					v[next.v] = true;
+					q.offer(next.v);
 				}
 			}
 
