694cea7c1b862472abbe37da9b0f5e3c2dada713
whyWhale <soccer_by@naver.com>
2024-08-18 14:48:32 +0900

    feat: ZOAC(success)

--- src/main/java/BaekJoon/tony/implementation/ZOAC.java
+++ src/main/java/BaekJoon/tony/implementation/ZOAC.java
@@ -3,53 +3,54 @@ package BaekJoon.tony.implementation;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Comparator;
-import java.util.PriorityQueue;
 
 public class ZOAC {
-	static class Part {
-
-		int order;
-		char ch;
-
-		public Part(int order, char ch) {
-			this.order = order;
-			this.ch = ch;
-		}
-	}
+	static StringBuilder answers = new StringBuilder();
+	static boolean[] isUsed;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		String s = reader.readLine();
-		Comparator<Part> dictionaryOrders = (a, b) -> {
-			if (a.ch == b.ch) {
-				return a.order - b.order;
-			}
+		isUsed = new boolean[s.length()];
 
-			return Character.compare(a.ch, b.ch);
-		};
-
-		PriorityQueue<Part> nexts = new PriorityQueue<>(dictionaryOrders);
-		int order = 0;
-		for (char next : s.toCharArray()) {
-			nexts.offer(new Part(order++, next));
-		}
-
-		String[] prints = new String[s.length()];
-		StringBuilder answer = new StringBuilder();
-		while (!nexts.isEmpty()) {
-			Part poll = nexts.poll();
-			prints[poll.order] = String.valueOf(poll.ch);
-
-			for (String value : prints) {
-				if (value != null && !value.isBlank()) {
-					answer.append(value);
-				}
-			}
-
-			answer.append(System.lineSeparator());
-		}
-
-		System.out.println(answer);
+		resolve(s, 0, s.length() - 1);
+		System.out.println(answers);
 	}
+
+	private static void resolve(String s, int start, int end) {
+		if (start > end) {
+			return;
+		}
+
+		char ch = ' ';
+		int selectIdx = -1;
+		for (int i = start; i <= end; i++) {
+			if (isUsed[i])
+				continue;
+			boolean isInitialize = ch == ' ' && selectIdx == -1;
+			if (isInitialize) {
+				ch = s.charAt(i);
+				selectIdx = i;
+				isUsed[i] = true;
+			}
+
+			if (Character.compare(ch, s.charAt(i)) > 0) {
+				ch = s.charAt(i);
+				isUsed[selectIdx] = false;
+				isUsed[i] = true;
+				selectIdx = i;
+			}
+		}
+
+		for (int i = 0; i < s.length(); i++) {
+			if (isUsed[i]) {
+				answers.append(s.charAt(i));
+			}
+		}
+
+		answers.append(System.lineSeparator());
+		resolve(s, selectIdx + 1, end);
+		resolve(s, start, selectIdx - 1);
+	}
+
 }
