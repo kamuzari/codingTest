64007e59c4549dab6605b1c290caf895b322dd9e
kamusari <soccer_by@naver.com>
2025-03-18 15:00:58 +0900

    feat(programmers): 데이터 분석 (enum 선언 없이 Arrays.asList, indexof 활용)

--- src/main/java/programmers/basic/AnalysisData.java
+++ src/main/java/programmers/basic/AnalysisData.java
@@ -1,33 +1,13 @@
 package programmers.basic;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class AnalysisData {
-	enum Colums {
-		CODE("code", 0),
-		DATE("date", 1),
-		MAXIMUM("maximum", 2),
-		REMAIL("remain", 3);
-
-		private String name;
-		private int index;
-
-		private Colums(String name, int index) {
-			this.name = name;
-			this.index = index;
-		}
-
-		public static Colums of(String name) {
-			return Arrays.stream(Colums.values())
-				.filter(col -> col.name.equals(name))
-				.findAny()
-				.orElseThrow(RuntimeException::new);
-		}
-	}
-
 	public int[][] solution(int[][] data, String ext, int val_ext, String sort_by) {
-		final int filterIndex = Colums.of(ext).index;
-		final int sortIndex = Colums.of(sort_by).index;
+		List<String> cols = Arrays.asList("code", "date", "maximum", "remain");
+		int filterIndex = cols.indexOf(ext);
+		int sortIndex = cols.indexOf(sort_by);
 		return Arrays.stream(data)
 			.filter(d -> d[filterIndex] < val_ext)
 			.sorted((a, b) -> a[sortIndex] - b[sortIndex])
